d1a38775759e4152fbe4cbdeb9a968db329d4925
thimoteus <thimoteus@teknik.io>
2018-02-06 16:58:04 -0800

    `intervalLength` now correctly calculates the number of available position identifiers available between two prefixes of a given size.

--- src/Logoot/Id.purs
+++ src/Logoot/Id.purs
@@ -7,42 +7,45 @@ import Control.Monad.Eff.Random (RANDOM)
 import Data.Array as A
 import Data.Foldable (class Foldable, length)
 import Data.Function (on)
-import Data.Int (Radix, toNumber)
+import Data.Int as Z
 import Data.Newtype (class Newtype, un)
-import Logoot.Types (IdentifierF(..), Position(..), digit)
+import Data.Ord (abs)
+import Logoot.Types (IdentifierF(IdentifierF), digit)
 import Math (pow, round)
 
 type IdGenerator m g f s c
   = IdentifierF f s c -> IdentifierF f s c -> Int -> Number -> s -> m (g (IdentifierF f s c))
 
--- logootRand :: forall e s c. IdGenerator (Eff (random :: RANDOM | e)) Array Array s c
--- logootRand p q n boundary s = pure []
-
-prefix
-  :: forall f s c . Foldable f => Functor f
-  => (forall a. Int -> f a -> f a) -> Base -> Int -> IdentifierF f s c -> Number
-prefix take base n = metric base <<< map digit <<< take n <<< un IdentifierF
+logootRand :: forall e s c. IdGenerator (Eff (random :: RANDOM | e)) Array Array s c
+logootRand p q n boundary s = pure []
 
 metric :: forall f. Foldable f => Base -> f Int -> Number
 metric (Base b) ds = (tidy (length ds) <<< _.val <<< A.foldl f {ind: 0, val: 0.0}) ds where
   f :: {ind :: Int, val :: Number} -> Int -> {ind :: Int, val :: Number}
-  f {ind, val} n = {ind: ind - 1, val: val + toNumber n*b^ind}
+  f {ind, val} n = {ind: ind - 1, val: val + Z.toNumber n*b^ind}
   tidy :: Int -> Number -> Number -- gets rid of pesky floating point errors
   tidy l x = round (x*b^l) / b^l
 
--- Need to turn identifiers into a metric: a function f :: Identifier -> Number
--- that satisfies the usual metric space properties
--- If f is invertible then we can project onto Numbers, operate on them, and pull
--- back.
+prefix
+  :: forall f s c . Foldable f => Functor f
+  => (forall a. Int -> f a -> f a)
+  -> Base -> Int -> IdentifierF f s c -> Number
+prefix take base n = metric base <<< map digit <<< take n <<< un IdentifierF
 
--- Can't we treat Array Int as a number by itself?
--- Assuming x :: Array Int maps to a Number f(x) \in [0, 1] where f^-1(0) is the
--- document start and f^-1(1) is the document's end.
+intervalLength
+  :: forall f s c. Foldable f => Functor f => (forall a. Int -> f a -> f a)
+  -> Base -> Int -> IdentifierF f s c -> IdentifierF f s c -> Int
+intervalLength take b@(Base b') n p q =
+  let
+    p' = prefix take b n p
+    q' = prefix take b n q
+  in
+    Z.round (abs (p'*b'^(n-1) - q'*b'^(n-1)) - 1.0)
 
 -- Utilities
 
 raise :: Int -> Int -> Number
-raise = pow `on` toNumber
+raise = pow `on` Z.toNumber
 
 infixr 8 raise as ^
 
--- test/Main.purs
+++ test/Main.purs
@@ -5,35 +5,44 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, error, log)
 import Data.Array as A
-import Logoot.Id (Base(..), prefix)
+import Logoot.Id (Base(..), prefix, intervalLength)
 import Logoot.Types (IdentifierF(..), Position(..))
 
 testPrefixes :: forall e. Eff (console :: CONSOLE | e) Unit
 testPrefixes = do
   log "Base: 100"
   log "prefix(p, 1) == 2"
-  print $ pref 1 p
+  pref 1 p ==? 2.0
   log "prefix(q, 1) == 10"
-  pref 1 q `shouldEq` 10.0
+  pref 1 q ==? 10.0
   log "prefix(p, 2) == 2.59"
-  pref 2 p `shouldEq` 2.59
+  pref 2 p ==? 2.59
   log "prefix(q, 2) == 10.20"
-  pref 2 q `shouldEq` 10.20
-  log "prefix(p, 3) == 2.59.0"
-  pref 3 p `shouldEq` 2.590
+  pref 2 q ==? 10.20
+  log "prefix(p, 3) == 2.59.00"
+  pref 3 p ==? 2.5900
   log "prefix(q, 3) == 10.20.03"
-  pref 3 q `shouldEq` 10.2003
+  pref 3 q ==? 10.2003
+  log "----"
+  log "intervalLength(p,q,1) == 7"
+  length 1 p q ==? 7
+  log "intervalLength(p,q,2) == 760"
+  length 2 p q ==? 760
+  log "intervalLength(p,q,3) == 76102"
+  length 3 p q ==? 76102
     where
     p = IdentifierF [Position 2 4 7, Position 59 9 5]
     q = IdentifierF [Position 10 5 3, Position 20 3 6, Position 3 3 9]
     b = Base 100
-    print = log <<< show
     pref = prefix A.take b
+    length = intervalLength A.take b
 
 shouldEq :: forall a e. Show a => Eq a => a -> a -> Eff (console :: CONSOLE | e) Unit
 shouldEq a b
   | a == b = log "Test passed"
   | otherwise = error $ "Failed on " <> show a <> " /= " <> show b
+
+infix 4 shouldEq as ==?
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
